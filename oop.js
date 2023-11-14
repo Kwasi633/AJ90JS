4cb05f0a748448279ecb85bdbee3bfc8d8176a4e
Kwasi633 <ezrakwasi633@gmail.com>
2023-11-14 18:42:05 +0000

    Updated content

--- oop.js
+++ oop.js
@@ -1,3 +1,4 @@
+/*
 //Defininig a class called Person
 class Person {
     constructor(name, place, days) {
@@ -16,5 +17,64 @@ class Person {
 
 const userOne = new Person("Ezra", "Ghana", 90);
 
-
 console.log(userOne.introduce()); 
+
+
+console.log("TASKS FOR TODAY:");
+for  (let i=0;i< toDo.length;i++){
+    console.log(toDo[i]);
+}
+*/
+
+const prompt = require('prompt-sync')();
+
+const overAllWinner = (input) => {
+    let players = {
+      kofi: 0,
+      ama: 0,
+      draw: 0,
+    };
+
+    input.forEach((a) => {
+        if (a === 1) {
+          players.kofi++;
+        } else if (a === 2) {
+          players.ama++;
+        } else if (a === 0) {
+          players.draw++;
+        }
+      }
+    )
+
+      if( players.kofi > players.ama){
+        console.log(`Kofi is the card game champion with ${players.kofi} wins`)
+      }
+      else if(players.kofi < players.ama){
+        console.log(`Ama is the champion with ${players.ama} wins`)
+      }
+
+      return players;
+}
+
+const gameScores = []
+let answer = "";
+
+while (answer !== "done") {
+  answer = prompt("Enter winner of this round or type done to finish: ");
+
+  const num = Number(answer)
+
+  if(answer !== "done"){
+    gameScores.push(num)
+  }
+}
+
+console.log("RESULTS");
+
+for  (let i=0;i< gameScores.length;i++){
+    console.log(gameScores[i]);
+}
+
+let cardGame = overAllWinner(gameScores)
+
+console.log(cardGame)
